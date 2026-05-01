e9c053a4629411107851362fbf611d2f6b3d7de7
Deniz Sevinç <denizsevinc343@gmail.com>
2026-05-01 16:20:05 +0300

    project routes is wrapped auth,admin middleware

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -2,7 +2,7 @@ export * from './models/enums'; // ? ok
 export * from './models/user'; // ? seed.ts dosyasında tanımlandı
 export * from "./models/heroSection"; // ? tamamlandı
 export * from "./models/aboutUs"; // ? tamamlandı
-export * from "./models/my_projects"; // TODO: Yapılacak
-export * from "./models/project_details"; // TODO: my_projects ile ilişkili
+export * from "./models/my_projects"; // ? tamamlandı
+export * from "./models/project_details"; // ? tamamlandı
 export * from "./models/blog"; // ? tamamlandı
 export * from "./models/my_info"; // ? tamammlandı
--- backend/src/modules/projects/projects.routes.ts
+++ backend/src/modules/projects/projects.routes.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import * as projectsController from "./projects.controller";
 import { upload } from "../../common/middleware/singleFileUpload";
+import { authMiddleware, adminMiddleware } from "../../common/middleware/auth.middleware";
 
 const router = Router();
 
-// To handle multiple files (cover_img and detail_imgs), we can use upload.fields
-// Since upload is exported as a multer instance, we can configure fields here:
-router.post("/", upload.fields([{ name: 'cover_img', maxCount: 1 }, { name: 'detail_imgs', maxCount: 10 }]), projectsController.createProjects);
+router.post("/", authMiddleware, adminMiddleware, upload.fields([{ name: 'cover_img', maxCount: 1 }, { name: 'detail_imgs', maxCount: 10 }]), projectsController.createProjects);
 router.get("/", projectsController.getAllProjects);
 router.get("/:id", projectsController.getProjectById);
-router.put("/:id", upload.fields([{ name: 'cover_img', maxCount: 1 }, { name: 'detail_imgs', maxCount: 10 }]), projectsController.updateProjectById);
-router.delete("/:id", projectsController.deleteProjectById);
+router.put("/:id", authMiddleware, adminMiddleware, upload.fields([{ name: 'cover_img', maxCount: 1 }, { name: 'detail_imgs', maxCount: 10 }]), projectsController.updateProjectById);
+router.delete("/:id", authMiddleware, adminMiddleware, projectsController.deleteProjectById);
 
 export default router;
